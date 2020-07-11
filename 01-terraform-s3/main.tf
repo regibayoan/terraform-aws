5006c35d529fb106cad1f09636a937a5a2df5c3f
regibayoan <regi.bayoan@gmail.com>
2020-07-11 23:44:33 +0100

    First commit

---
+++ 01-terraform-s3/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = var.aws_region
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+}
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "terraform-s3-regi"
+    }
